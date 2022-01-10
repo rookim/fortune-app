1ce8192d96da8122ed4d2a968a099562b571352a
ro <kronew6@gmail.com>
2022-01-10 13:48:24 -0600

    adds random_fortunes and lotto_wins methods to MyExamplesController

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,20 @@
 class MyExamplesController < ApplicationController
+  def random_fortunes
+    array = ["You'll get a job as a software developer in the near future!", "You'll graduate from Actualize Coding Bootcamp!", "You'll fall more in love with programming!"]
+    number = rand(3)
+    render json: { "Your fortune": array[number] }
+  end
+
+  def lotto_win
+    array = []
+    6.times.each do
+      x = rand(1..60)
+      array << x
+    end
+
+    render json: array
+  end
+
+  def visits
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # generate random fortune
+  get "/my_fortune", controller: "my_examples", action: "random_fortunes"
+
+  # return 6 random numbers
+  get "/predict_lotto", controller: "my_examples", action: "lotto_win"
 end
